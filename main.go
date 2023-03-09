9fbcf17c630a9f5e2c3b6134a4d2ccc221b408ee
likeyao <likeyao777@gmail.com>
2023-03-10 01:27:37 +0800

    读取文件夹中的文章内容

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/gin-gonic/gin"
 	"github.com/likeyao/peach-blog/pkg/controller"
 )
 
 func main() {
+	var path string
+
+	flag.StringVar(&path, "p", "/Users/likeyao/demo/blogs", "存放博客文章的文件夹")
+	flag.Parse()
+
+	index := controller.IndexController{Path: path}
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
-	r.GET("/", controller.Index)
+	r.GET("/", index.Index)
 	r.GET("/article/detail", controller.ArticelDetail)
 	r.Static("/assets", "assets")
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+//要能够根据配置，去某个文件目录中读取所有的博客内容，文件名作为标题、读取文件中的一段内容作为预览内容
--- pkg/controller/index.go
+++ pkg/controller/index.go
@@ -2,12 +2,71 @@ package controller
 
 import (
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Index(c *gin.Context) {
+type IndexController struct {
+	Path string
+}
+
+func (i *IndexController) Index(c *gin.Context) {
+	//试一下读取文件名
+	articles := readArticles(i.Path)
+
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"list": []string{"测试Blog标题一", "测试Blog标题二", "测试Blog标题三"},
+		"list": articles,
 	})
+}
+
+func readArticles(path string) []article {
+	result := make([]article, 0)
+	dirs, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range dirs {
+		fullPath := path + "/" + v.Name()
+		if !v.IsDir() {
+			result = append(result, article{
+				Name:    parseAritcleTitle(v.Name()),
+				Content: readArticle(fullPath),
+				Outline: readArticle(fullPath),
+				Time:    parseTime(v),
+			})
+		}
+
+	}
+	return result
+}
+
+func parseAritcleTitle(filename string) string {
+	return filename[:strings.Index(filename, ".")]
+}
+
+func readArticle(filename string) string {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(content)
+}
+
+func parseTime(dir os.DirEntry) string {
+	info, err := dir.Info()
+	if err != nil {
+		panic(err)
+	}
+	return info.ModTime().Format("2006-01-02 15:04:05")
+}
+
+type article struct {
+	Name    string
+	Content string
+	Outline string
+	Time    string
 }
